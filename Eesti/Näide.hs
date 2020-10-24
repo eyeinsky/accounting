5ca262069f983f74f621ddd44c1e4b176369f368
Markus Läll <markus.l2ll@gmail.com>
2020-10-24 19:57:11 +0300

    Improve Eesti.Näide

--- Eesti/Näide.hs
+++ Eesti/Näide.hs
@@ -6,34 +6,44 @@ import Eesti
 
 -- * Kontod
 
+vara, kohustis, tulu', kulu' :: Int -> String -> Account
+vara n kirjeldus = Account (100 + n) kirjeldus
+kohustis n kirjeldus = Account (200 + n) kirjeldus
+tulu' n kirjeldus = Account (400 + n) kirjeldus
+kulu' n kirjeldus = Account (500 + n) kirjeldus
+
 pank, sisendKM, kassa, võlgMulle, ettemaksukonto, väljundKM, käibemaks, võlg, omakapital, tulu, kulu, palgakulu, sotsiaalmaks :: Account
 
+
 -- | Varad (1)
-pank = Account 1 "Pank"
-sisendKM = Account 1 "Sisendkäibemaks"
-kassa = Account 1 "Kassa"
-võlgMulle = Account 1 "Võlg mulle"
-ettemaksukonto = Account 1 "Tax office prepaid"
+pank = vara 0 "Pank"
+sisendKM = vara 1 "Sisendkäibemaks"
+kassa = vara 2 "Kassa"
+võlgMulle = vara 3 "Võlg mulle"
+ettemaksukonto = vara 4 "Tax office prepaid"
 
 -- | Kohustised (2)
-väljundKM = Account 2 "Väljundkäibemaks"
-käibemaks = Account 2 "Käibemaks (käibemaksuvõlg riigile)"
-võlg = Account 2 "Võlg (kellelegi teisele)"
+väljundKM = kohustis 0 "Väljundkäibemaks"
+käibemaks = kohustis 1 "Käibemaks (käibemaksuvõlg riigile)"
+võlg = kohustis 2 "Võlg (kellelegi teisele)"
 
 -- | Omakapital (3)
 omakapital = Account 3 "Omakapital"
 
 -- | Tulud (4)
-tulu = Account 4 "Tulu"
+tulu = tulu' 0 "Tulu"
 
 -- | Kulud (5)
-kulu = Account 5 "Kulu"
-palgakulu = Account 5 "Palgakulu"
-sotsiaalmaks = Account 5 "Sotsiaalmaks"
+kulu = kulu' 0 "Kulu"
+palgakulu = kulu' 1 "Palgakulu"
+sotsiaalmaks = kulu' 2 "Sotsiaalmaks"
 
 -- * Annotatsioon
 
-data Annotation = Annotation { description :: String } deriving Show
+data Annotation = Annotation { description :: String }
+
+instance Show Annotation where
+  show (Annotation k) = show k
 
 instance Semigroup Annotation where
   _ <> a = a
@@ -46,8 +56,8 @@ kirjeldus text = annotate $ Annotation text
 
 -- * Tehingud
 
-transactions :: [Transaction Day Annotation]
-transactions = execT $ do
+tehingud :: [Transaction Day Annotation]
+tehingud = execT $ do
 
   at (stringDate "2020-01-15") $ do
     kirjeldus "Omakapital"
@@ -60,7 +70,7 @@ transactions = execT $ do
     võlgMulle ~> pank $ 4800
 
   at (stringDate "2020-02-20") $ do
-    kirjeldus "Ost"
+    kirjeldus "Ost: mingi asi"
     võlg ~> kulu $ 300
     võlg ~> sisendKM $ 60
   at (stringDate "2020-02-24") $ do
@@ -73,7 +83,23 @@ transactions = execT $ do
     pank ~> ettemaksukonto $ 740
     ettemaksukonto ~> käibemaks $ 740
 
--- * Käibemaksu deklaratsioonid
+-- * Raportid
 
-kmd :: IO ()
-kmd = _KMDd sisendKM väljundKM transactions
+main :: IO ()
+main = do
+  nl
+
+  -- | Käibemaksudeklaratsioonid
+  _KMDd sisendKM väljundKM tehingud
+  nl
+
+  -- | Käibemaksu kontode saldo
+  tekst "Käibemaksukontod:"
+  kontosaldo sisendKM tehingud
+  kontosaldo väljundKM tehingud
+  kontosaldo käibemaks tehingud
+  nl
+
+  -- | Pangasaldo
+  kontosaldo pank tehingud
+  nl
