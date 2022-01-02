938d9ead82d1ab3f166489c7212e4b8ec5a785d5
Markus Läll <markus.l2ll@gmail.com>
2022-01-02 22:02:55 +0200

    Add aastaaruanded to näide

--- Eesti/Näide.hs
+++ Eesti/Näide.hs
@@ -3,12 +3,14 @@ module Eesti.Näide where
 import LocalPrelude
 import Accounting
 import Eesti
+import Eesti.Aastaaruanne
 
 -- * Kontod
 
-vara, kohustis, tulu', kulu' :: Int -> String -> Account
+vara, kohustis, omakapital', tulu', kulu' :: Int -> String -> Account
 vara n kirjeldus = Account (100 + n) kirjeldus
 kohustis n kirjeldus = Account (200 + n) kirjeldus
+omakapital' n kirjeldus = Account (300 + n) kirjeldus
 tulu' n kirjeldus = Account (400 + n) kirjeldus
 kulu' n kirjeldus = Account (500 + n) kirjeldus
 
@@ -28,7 +30,13 @@ käibemaks = kohustis 1 "Käibemaks (käibemaksuvõlg riigile)"
 võlg = kohustis 2 "Võlg (kellelegi teisele)"
 
 -- | Omakapital (3)
-omakapital = Account 3 "Omakapital"
+omakapital = omakapital' 1 "Omakapital"
+
+ktKoond :: Account
+ktKoond = omakapital' 2 "Koondkonto"
+
+aruandeperioodiKasum :: Account
+aruandeperioodiKasum = omakapital' 3 "Aruandeperioodi kasum"
 
 -- | Tulud (4)
 tulu = tulu' 0 "Tulu"
@@ -37,6 +45,9 @@ tulu = tulu' 0 "Tulu"
 kulu = kulu' 0 "Kulu"
 palgakulu = kulu' 1 "Palgakulu"
 sotsiaalmaks = kulu' 2 "Sotsiaalmaks"
+
+tööjõukulud :: [Account]
+tööjõukulud = [palgakulu, sotsiaalmaks]
 
 -- * Annotatsioon
 
@@ -62,6 +73,8 @@ kirjeldus text = annotate $ Annotation $ Just text
 
 tehingud :: [Transaction Day Annotation]
 tehingud = execT $ do
+
+  -- * 2020
 
   at (stringDate "2020-01-15") $ do
     kirjeldus "Omakapital"
@@ -97,6 +110,38 @@ tehingud = execT $ do
     -- | Käibemaksuvõlg ja ettemaks lahenduvad nulliks
     ettemaksukonto ~> käibemaks $ 735.6
 
+  -- * 2021
+
+  at (stringDate "2021-02-15") $ do
+    kirjeldus "Müük: teenuse osutamine"
+    tulu ~> võlgMulle $ 5000
+    väljundKM ~> võlgMulle $ 1000
+    võlgMulle ~> pank $ 6000
+
+  at (stringDate "2021-02-20") $ do
+    kirjeldus "Ost: arvega ostetud asi, nt monitor"
+    võlg ~> kulu $ 400
+    võlg ~> sisendKM $ 80
+  at (stringDate "2021-02-24") $ do
+    pank ~> võlg $ 480
+
+  at (stringDate "2021-02-25") $ do
+    kirjeldus "Ost: teine, kohe makstud asi"
+    võlg ~> kulu $ 50
+    võlg ~> sisendKM $ 10
+    pank ~> võlg $ 60
+
+  at (stringDate "2021-03-15") $ do
+    kirjeldus "Käibemaks: 2021-02"
+    -- | Kanname käibemaksu konto @käibemaks@ peale kokku. Sisend ja
+    -- väljund on peale seda nullis.
+    sisendKM ~> käibemaks $ 90
+    käibemaks ~> väljundKM $ 1000
+    -- | Kanname ettemaksu maksuametisse
+    pank ~> ettemaksukonto $ 910
+    -- | Käibemaksuvõlg ja ettemaks lahenduvad nulliks
+    ettemaksukonto ~> käibemaks $ 910
+
 -- * Raportid
 
 main :: IO ()
@@ -116,3 +161,10 @@ main = do
   -- | Pangasaldo
   kontosaldo pank tehingud
   nl
+
+
+  nl
+  let
+    lõpukandeAnnotatsioon :: Integer -> Maybe Annotation
+    lõpukandeAnnotatsioon aasta = Just $ Annotation $ Just $ show aasta <> " lõpukanne"
+  printAastaaruanded lõpukandeAnnotatsioon ktKoond aruandeperioodiKasum tööjõukulud tehingud
